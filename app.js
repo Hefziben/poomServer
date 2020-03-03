1c0f2fb43164c15ab246579626fddc653c8eb4e9
milton <hefziben@gmail.com>
2020-03-03 00:41:02 -0500

    cors

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+app.use(cors({
+  origin: 'https://binndy.com' 
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
