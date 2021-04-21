30abc9e3b034b3b6d0bbef3ae67d05b56344b251
milton <hefziben@gmail.com>
2021-04-21 15:39:54 -0500

    update

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(cors({
+  origin: ["https://panel.poomapp.com","https://poomapp.com"] 
+  
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
