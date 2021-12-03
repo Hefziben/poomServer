350742a7ed1f8f9fbca5d2ae1e99682a100c7599
hefziben <hefziben@gmail.com>
2021-12-03 14:26:34 -0500

    encrypt comercio passwords

--- routes/comercio.js
+++ routes/comercio.js
@@ -239,7 +239,7 @@ router.post("/login/", function(req, res, next) {
       if (response) {
         bcrypt.compare(cliente.password, response.password, function(err, result) {
           if (result) {
-            User.findOne({telefono:cliente.telefono},{ password: 0}, (err, clienteFilter) => {
+            Comercio.findOne({telefono:cliente.telefono},{ password: 0}, (err, clienteFilter) => {
                 // generar token
               const accessToken = jwt.sign({ user: clienteFilter.telefono, role:response.role }, process.env.TOKEN_SECRET,{ expiresIn: '86400s' });
               res.send({ mensaje: "Success", token:accessToken, data: clienteFilter});
@@ -275,23 +275,23 @@ router.post("/verify/", function(req, res, next) {
   });
 });
 
-router.get("/updatePasswords/:id", (req, res) => {
-  Comercio.find({}, (err, users) => {
-    console.log(err);
-    if (res.status == 400) {
-      res.send({ mensaje: "error in get request", res: err });
-    } else {
-    users.forEach(item =>{
-      bcrypt.hash('Contrasena', saltRounds, function(err, hash) {
-        item.password = hash;
-        console.log(item.password);
-        Comercio.findByIdAndUpdate(item._id, { $set: item }, { new: true })
-        .then(() => console.log('Contrasena cambiada con exito'))
-        .catch(err => res.status(400).send(err));
-     });
-    })
-    }
-  });
-})
+// router.get("/updatePasswords/:id", (req, res) => {
+//   Comercio.find({}, (err, users) => {
+//     console.log(err);
+//     if (res.status == 400) {
+//       res.send({ mensaje: "error in get request", res: err });
+//     } else {
+//     users.forEach(item =>{
+//       bcrypt.hash('Contrasena', saltRounds, function(err, hash) {
+//         item.password = hash;
+//         console.log(item.password);
+//         Comercio.findByIdAndUpdate(item._id, { $set: item }, { new: true })
+//         .then(() => console.log('Contrasena cambiada con exito'))
+//         .catch(err => res.status(400).send(err));
+//      });
+//     })
+//     }
+//   });
+// })
 
 module.exports = router;
