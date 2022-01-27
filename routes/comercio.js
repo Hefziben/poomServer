81e6b469ab56875c14078d152bdad05b6be22736
hefziben <hefziben@gmail.com>
2022-01-26 21:16:56 -0500

    password change modi

--- routes/comercio.js
+++ routes/comercio.js
@@ -263,6 +263,7 @@ router.post("/login", function(req, res, next) {
 router.put("/password/:id", (req, res) => {
   const comercioId = req.params.id;
   const comercio = req.body;
+  console.log(comercio.password);
   bcrypt.hash(comercio.password, saltRounds, function(err, hash) {
    comercio.password = hash;
    console.log(comercio.password);
